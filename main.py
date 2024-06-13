718a0cfc657c6659bebb83513bdab21c217f5573
jordanaescalona <jordana.escalona@gmail.com>
2024-06-13 11:33:32 -0300

    Subiendo archivo principal

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+
+import discord
+import requests
+
+
+TOKEN = os.environ['TOKEN']
+
+# Configuración del cliente de Discord
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+
+def get_joke():
+    response = requests.get("https://v2.jokeapi.dev/joke/Any")
+    if response.status_code == 200:
+        joke_data = response.json()
+        if joke_data['type'] == 'single':
+            return joke_data['joke']
+        else:
+            return f"{joke_data['setup']} ... {joke_data['delivery']}"
+    else:
+        return "No pude encontrar un chiste en este momento, intenta más tarde."
+
+@client.event
+async def on_ready():
+    print(f'Bot conectado como {client.user}')
+
+@client.event
+async def on_message(message):
+    
+    if message.author == client.user:
+        return
+    
+    if message.content.lower().startswith('hello'):
+        await message.channel.send(f'¡Hello, {message.author.name}!Do you want me to tell you a joke?please type *!joke*')
+    
+    if message.content.lower().startswith('!joke'):
+        joke = get_joke()
+        await message.channel.send(joke)
+
+# Iniciar el bot
+client.run(TOKEN)
